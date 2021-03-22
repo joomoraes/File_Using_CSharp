67b3f96841f94d4d1138654257a3eb78d7644a33
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2021-03-22 11:10:38 -0400

    Add files via upload

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace streamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\joao.alves\source\repos\FileUsing\archivements\lorem.txt";
+            string targetPath = @"C:\Users\joao.alves\source\repos\FileUsing\archivements\lorem2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            } catch(IOException e)
+            {
+                Console.WriteLine("Um Erro Foi Encoontrado!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
