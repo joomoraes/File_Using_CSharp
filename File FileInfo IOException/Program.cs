2fc3501baa38b648a2cb190be6c7b42101873da3
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2021-03-22 10:49:49 -0400

    Add files via upload

---
+++ File FileInfo IOException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\joao.alves\source\repos\FileUsing\archivements\lorem.txt";
+            string targetPath = @"C:\Users\joao.alves\source\repos\FileUsing\archivements\lorem2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            } catch(IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
