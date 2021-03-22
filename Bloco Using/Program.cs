defcd067586392945b08307bda93d5fe681e0064
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2021-03-22 11:04:38 -0400

    Add files via upload

---
+++ Bloco Using/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace usingBock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\joao.alves\source\repos\FileUsing\FileStream\lorem.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro foi encontrado");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
