9f5f2195e00364676056cca2d102290d1274156e
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2021-03-22 10:56:57 -0400

    Add files via upload

---
+++ FileStream/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace fileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\joao.alves\source\repos\FileUsing\archivements\lorem.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            } catch(IOException e)
+            {
+                Console.WriteLine("Um erro Ocorreu");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
