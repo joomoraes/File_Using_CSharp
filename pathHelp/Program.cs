c5257f3a477d3b90ce17217c10a240cf65efa5c4
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2021-03-22 11:27:56 -0400

    Add files via upload

---
+++ pathHelp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\joao.alves\source\repos\FileUsing\archivements/lorem.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " +  Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path);
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath" + Path.GetTempPath());
+        }
+    }
+}
